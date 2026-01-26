edb688ffc94c0cd6c5d8fb2c167fede0f6662684
Daniel <danielcederberg1@gmail.com>
2026-01-26 09:24:28 -0800

    output

--- tests/test_dton.h
+++ tests/test_dton.h
@@ -1703,7 +1703,9 @@ static const char *all_tests_dton()
     //   mu_run_test(test_12_dton, counter_dton);
     mu_run_test(test_13_dton, counter_dton); // implemented
     mu_run_test(test_14_dton, counter_dton); // implemented
+    printf("before test 15\n");
     mu_run_test(test_15_dton, counter_dton); // implemented
+    printf("after test 15\n");
     mu_run_test(test_16_dton, counter_dton); // implemented
     mu_run_test(test_17_dton, counter_dton); // implemented
     mu_run_test(test_18_dton, counter_dton); // implemented
