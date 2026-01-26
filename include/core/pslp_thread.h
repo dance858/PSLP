ff5594580cd677a2b5fd9dc5cc1835e0cb136265
Daniel <danielcederberg1@gmail.com>
2026-01-26 10:12:14 -0800

    windows threads

--- include/core/pslp_thread.h
+++ include/core/pslp_thread.h
@@ -2,33 +2,68 @@
 #define PSLP_THREAD_H
 
 #if defined(_WIN32) || defined(_WIN64)
+#include <stdint.h>
 #include <windows.h>
+// Portable thread type
 typedef HANDLE ps_thread_t;
-typedef DWORD(WINAPI *ps_thread_func_t)(LPVOID);
+
+// Internal structure to pass user function and argument
+typedef struct
+{
+    void *(*start_routine)(void *);
+    void *arg;
+    void *ret;
+} ps_thread_wrapper_t;
+
+// Windows thread wrapper to safely return pointer
+static DWORD WINAPI ps_thread_start_wrapper(LPVOID param)
+{
+    ps_thread_wrapper_t *wrapper = (ps_thread_wrapper_t *) param;
+    wrapper->ret = wrapper->start_routine(wrapper->arg);
+    return 0;
+}
+
+// Thread create: attr is ignored on Windows
 static inline int ps_thread_create(ps_thread_t *thread, void *attr,
                                    void *(*start_routine)(void *), void *arg)
 {
-    (void) attr;
-    *thread =
-        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) start_routine, arg, 0, NULL);
+    (void) attr; // Not used on Windows
+    ps_thread_wrapper_t *wrapper =
+        (ps_thread_wrapper_t *) malloc(sizeof(ps_thread_wrapper_t));
+    if (!wrapper) return -1;
+    wrapper->start_routine = start_routine;
+    wrapper->arg = arg;
+    wrapper->ret = NULL;
+    *thread = CreateThread(NULL, 0, ps_thread_start_wrapper, wrapper, 0, NULL);
     return *thread ? 0 : -1;
 }
+
+// Thread join: retrieves pointer return value if possible
 static inline int ps_thread_join(ps_thread_t thread, void **retval)
 {
     DWORD res = WaitForSingleObject(thread, INFINITE);
     if (res == WAIT_OBJECT_0)
     {
-        if (retval)
+        // Retrieve wrapper and return value
+        ps_thread_wrapper_t *wrapper = NULL;
+        BOOL ok = GetExitCodeThread(thread, (LPDWORD) &wrapper);
+        if (ok && wrapper)
         {
-            DWORD code;
-            GetExitCodeThread(thread, &code);
-            *retval = (void *) (uintptr_t) code;
+            if (retval) *retval = wrapper->ret;
+            free(wrapper);
+        }
+        else
+        {
+            if (retval) *retval = NULL;
         }
         CloseHandle(thread);
         return 0;
     }
     return -1;
 }
+
+// Thread cancel: WARNING - unsafe, does not clean up resources!
+// Use only if absolutely necessary. Prefer cooperative cancellation.
 static inline int ps_thread_cancel(ps_thread_t thread)
 {
     TerminateThread(thread, 0);
