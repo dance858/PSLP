0dd09cf9b799e9ef50cbaeac98b7cdcf298dad8f
Daniel <danielcederberg1@gmail.com>
2026-01-26 10:55:39 -0800

    window threads

--- include/core/pslp_thread.h
+++ include/core/pslp_thread.h
@@ -7,76 +7,63 @@
 #include <stdlib.h>
 #include <windows.h>
 
-/* ---------------- Windows Thread Types ---------------- */
-typedef HANDLE ps_thread_t;
-
-/* Wrapper structure for POSIX-style thread on Windows */
 typedef struct
 {
     void *(*start_routine)(void *);
     void *arg;
     void *ret;
-    volatile LONG cancel_requested; /* cooperative cancellation flag */
 } ps_thread_wrapper_t;
 
-/* Thread trampoline: calls the user function */
+typedef struct
+{
+    HANDLE handle;
+    ps_thread_wrapper_t *wrapper;
+} ps_thread_t;
+
+typedef struct
+{
+    HANDLE handle;
+    ps_thread_wrapper_t *wrapper;
+} ps_thread_t;
+
 static DWORD WINAPI ps_thread_trampoline(LPVOID param)
 {
-    ps_thread_wrapper_t *w = (ps_thread_wrapper_t *) param;
-    if (!w->cancel_requested)
-    {
-        w->ret = w->start_routine(w->arg);
-    }
+    ps_thread_wrapper_t *w = param;
+    w->ret = w->start_routine(w->arg);
     return 0;
 }
 
-/* Create a thread */
-static inline int ps_thread_create(ps_thread_t *thread,
-                                   void *attr, /* ignored on Windows */
+static inline int ps_thread_create(ps_thread_t *t, void *attr,
                                    void *(*start_routine)(void *), void *arg)
 {
     (void) attr;
 
-    ps_thread_wrapper_t *w = malloc(sizeof(*w));
-    if (!w) return -1;
+    t->wrapper = malloc(sizeof(*t->wrapper));
+    if (!t->wrapper) return -1;
 
-    w->start_routine = start_routine;
-    w->arg = arg;
-    w->ret = NULL;
-    w->cancel_requested = 0;
+    t->wrapper->start_routine = start_routine;
+    t->wrapper->arg = arg;
+    t->wrapper->ret = NULL;
 
-    *thread = CreateThread(NULL, 0, ps_thread_trampoline, w, 0, NULL);
+    t->handle = CreateThread(NULL, 0, ps_thread_trampoline, t->wrapper, 0, NULL);
 
-    if (!*thread)
+    if (!t->handle)
     {
-        free(w);
+        free(t->wrapper);
         return -1;
     }
 
     return 0;
 }
 
-/* Join a thread */
-static inline int ps_thread_join(ps_thread_t thread, void **retval)
+static inline int ps_thread_join(ps_thread_t t, void **retval)
 {
-    if (WaitForSingleObject(thread, INFINITE) != WAIT_OBJECT_0) return -1;
+    if (WaitForSingleObject(t.handle, INFINITE) != WAIT_OBJECT_0) return -1;
 
-    /* Recover wrapper pointer */
-    ps_thread_wrapper_t *w = NULL;
-    GetExitCodeThread(thread, (LPDWORD) &w);
+    if (retval) *retval = t.wrapper->ret;
 
-    if (retval && w) *retval = w->ret;
-
-    free(w);
-    CloseHandle(thread);
-    return 0;
-}
-
-/* Cooperative cancel: signal the thread to exit at next check */
-static inline int ps_thread_cancel(ps_thread_wrapper_t *w)
-{
-    if (!w) return -1;
-    InterlockedExchange(&w->cancel_requested, 1);
+    CloseHandle(t.handle);
+    free(t.wrapper);
     return 0;
 }
 
