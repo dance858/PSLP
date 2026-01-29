bf66bae14e8ed74da6a1ebecad83ae85eeac6fda
Daniel <danielcederberg1@gmail.com>
2026-01-28 18:21:46 -0800

    add more tests

--- tests/all_tests.c
+++ tests/all_tests.c
@@ -3,9 +3,9 @@
 #include "test_Matrix.h"
 //  #include "test_Parallel_cols.h"
 //  #include "test_Parallel_rows.h"
-//  #include "test_Presolver.h"
+#include "test_Presolver.h"
 #include "test_SimpleReductions.h"
-//  #include "test_domain_propagation.h"
+#include "test_domain_propagation.h"
 #include "test_dton.h"
 #include "test_iVec.h"
 #include "test_pathological.h"
@@ -22,10 +22,10 @@ const char *run_all_tests()
     mu_assert("ston error", test_ston());
     //     mu_assert("parallel_rows error", test_parallel_rows());
     mu_assert("simple reductions error", test_simple());
-    //  mu_assert("domain propagation error", test_domain());
+    mu_assert("domain propagation error", test_domain());
     //     mu_assert("parallel_cols error", test_parallel_cols());
     //     mu_assert("postsolve error", test_postsolve());
-    //     mu_assert("presolver error", test_presolver());
+    mu_assert("presolver error", test_presolver());
     mu_assert("pathological error", test_pathological());
 
     return NULL;
