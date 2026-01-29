3696e496950d6951c182c224c97c500f74fc4bf9
Daniel <danielcederberg1@gmail.com>
2026-01-28 18:18:19 -0800

    add more tests

--- tests/all_tests.c
+++ tests/all_tests.c
@@ -1,23 +1,23 @@
-// #include "test_Constraints.h"
+#include "test_Constraints.h"
 // #include "test_CoreTransformations.h"
-// #include "test_Matrix.h"
+#include "test_Matrix.h"
 //  #include "test_Parallel_cols.h"
 //  #include "test_Parallel_rows.h"
 //  #include "test_Presolver.h"
 //  #include "test_SimpleReductions.h"
 //  #include "test_domain_propagation.h"
-//  #include "test_dton.h"
-// #include "test_iVec.h"
+#include "test_dton.h"
+#include "test_iVec.h"
 #include "test_pathological.h"
 //  #include "test_postsolve.h"
 // #include "test_ston.h"
 
 const char *run_all_tests()
 {
-    // mu_assert("matrix error", test_matrix());
-    // mu_assert("constraints error", test_constraints());
-    // mu_assert("iVec error", test_iVec());
-    //  mu_assert("dton error", test_dton());
+    mu_assert("matrix error", test_matrix());
+    mu_assert("constraints error", test_constraints());
+    mu_assert("iVec error", test_iVec());
+    mu_assert("dton error", test_dton());
     //    mu_assert("core error", test_core());
     //    mu_assert("ston error", test_ston());
     //     mu_assert("parallel_rows error", test_parallel_rows());
